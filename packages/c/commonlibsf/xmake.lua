2e600ad358395de95bbcb52c89b3c56b3f5c92e8
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2024-09-06 19:51:23 -0600

    add commonlibsf, figcone

---
+++ packages/c/commonlibsf/xmake.lua
@@ -0,0 +1,33 @@
+package("commonlibsf")
+    set_homepage("https://github.com/shad0wshayd3-SF1/CommonLibSF")
+    set_description("A collaborative reverse-engineered library for Starfield")
+    set_license("GPL-3.0")
+
+    add_urls("https://github.com/shad0wshayd3-SF1/CommonLibSF.git")
+
+    add_configs("sfse_xbyak", {description = "Enable trampoline support for Xbyak", default = false, type = "boolean"})
+
+    add_deps("spdlog", { configs = { header_only = false, std_format = true } })
+
+    add_syslinks("advapi32", "dbghelp", "ole32", "shell32", "user32", "version", "ws2_32")
+
+    on_load("windows|x64", function(package)
+        if package:config("sfse_xbyak") then
+            package:add("defines", "SFSE_SUPPORT_XBYAK=1")
+            package:add("deps", "xbyak")
+        end
+    end)
+
+    on_install("windows|x64", function(package)
+        import("package.tools.xmake").install(package, {
+            sfse_xbyak = package:config("sfse_xbyak")
+        }, { target = "commonlibsf" })
+    end)
+
+    on_test("windows|x64", function(package)
+        assert(package:check_cxxsnippets({test = [[
+            SFSEPluginLoad(const SFSE::LoadInterface*) {
+                return true;
+            };
+        ]]}, { configs = { languages = "c++23" }, includes = "SFSE/SFSE.h" }))
+    end)
---
+++ packages/f/figcone/xmake.lua
@@ -0,0 +1,30 @@
+package("figcone")
+    set_homepage("https://github.com/kamchatka-volcano/figcone")
+    set_description("Read JSON, YAML, TOML, XML or INI configuration by declaring a struct")
+    set_license("MS-PL")
+
+    add_urls("https://github.com/kamchatka-volcano/figcone.git")
+
+    add_versions("3.1.0", "")
+
+    add_configs("use_json", {description = "enable json parsing", default = false, type = "boolean"})
+    add_configs("use_yaml", {description = "enable yaml parsing", default = false, type = "boolean"})
+    add_configs("use_toml", {description = "enable toml parsing", default = false, type = "boolean"})
+    add_configs("use_ini", {description = "enable ini parsing", default = false, type = "boolean"})
+    add_configs("use_xml", {description = "enable xml parsing", default = false, type = "boolean"})
+    add_configs("use_shoal", {description = "enable shoal parsing", default = false, type = "boolean"})
+
+    add_deps("cmake")
+
+    on_install("windows|x64", function(package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DFIGCONE_USE_JSON=" .. (package:config("use_json") and "ON" or "OFF"))
+        table.insert(configs, "-DFIGCONE_USE_YAML=" .. (package:config("use_yaml") and "ON" or "OFF"))
+        table.insert(configs, "-DFIGCONE_USE_TOML=" .. (package:config("use_toml") and "ON" or "OFF"))
+        table.insert(configs, "-DFIGCONE_USE_INI=" .. (package:config("use_ini") and "ON" or "OFF"))
+        table.insert(configs, "-DFIGCONE_USE_XML=" .. (package:config("use_xml") and "ON" or "OFF"))
+        table.insert(configs, "-DFIGCONE_USE_SHOAL=" .. (package:config("use_shoal") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    end)
